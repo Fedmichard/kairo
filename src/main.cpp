fe8960c411cc745c0c7b770ae3f02c3c6718ee33
Fedmichard <francoisfedmichard@gmail.com>
2026-09-22 21:40:29 -0400

    Added Raygui to the application, getting a warning for some unused function inside the header file. IDC. Time to code

--- src/main.cpp
+++ src/main.cpp
@@ -1,108 +1,36 @@
 #include "C:/raylib/raylib/src/raylib.h"
 
-#define MAX_GESTURE_STRINGS   20
+#define RAYGUI_IMPLEMENTATION
+#include "../libs/raygui.h"
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
-int main(void)
+int main()
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 1000;
-    const int screenHeight = 450;
+    InitWindow(400, 200, "raygui - controls test suite");
+    SetTargetFPS(60);
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - input gestures");
+    bool showMessageBox = false;
 
-    Vector2 touchPosition = { 0, 0 };
-    Rectangle touchArea = { 220, 10, screenWidth - 230.0f, screenHeight - 20.0f };
-
-    int gesturesCount = 0;
-    char gestureStrings[MAX_GESTURE_STRINGS][32];
-
-    int currentGesture = GESTURE_NONE;
-    int lastGesture = GESTURE_NONE;
-
-    //SetGesturesEnabled(0b0000000000001001);   // Enable only some gestures to be detected
-
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    while (!WindowShouldClose())
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        lastGesture = currentGesture;
-        currentGesture = GetGestureDetected();
-        touchPosition = GetTouchPosition(0);
-
-        if (CheckCollisionPointRec(touchPosition, touchArea) && (currentGesture != GESTURE_NONE))
-        {
-            if (currentGesture != lastGesture)
-            {
-                // Store gesture string
-                switch (currentGesture)
-                {
-                    case GESTURE_TAP: TextCopy(gestureStrings[gesturesCount], "GESTURE TAP"); break;
-                    case GESTURE_DOUBLETAP: TextCopy(gestureStrings[gesturesCount], "GESTURE DOUBLETAP"); break;
-                    case GESTURE_HOLD: TextCopy(gestureStrings[gesturesCount], "GESTURE HOLD"); break;
-                    case GESTURE_DRAG: TextCopy(gestureStrings[gesturesCount], "GESTURE DRAG"); break;
-                    case GESTURE_SWIPE_RIGHT: TextCopy(gestureStrings[gesturesCount], "GESTURE SWIPE RIGHT"); break;
-                    case GESTURE_SWIPE_LEFT: TextCopy(gestureStrings[gesturesCount], "GESTURE SWIPE LEFT"); break;
-                    case GESTURE_SWIPE_UP: TextCopy(gestureStrings[gesturesCount], "GESTURE SWIPE UP"); break;
-                    case GESTURE_SWIPE_DOWN: TextCopy(gestureStrings[gesturesCount], "GESTURE SWIPE DOWN"); break;
-                    case GESTURE_PINCH_IN: TextCopy(gestureStrings[gesturesCount], "GESTURE PINCH IN"); break;
-                    case GESTURE_PINCH_OUT: TextCopy(gestureStrings[gesturesCount], "GESTURE PINCH OUT"); break;
-                    default: break;
-                }
-
-                gesturesCount++;
-
-                // Reset gestures strings
-                if (gesturesCount >= MAX_GESTURE_STRINGS)
-                {
-                    for (int i = 0; i < MAX_GESTURE_STRINGS; i++) TextCopy(gestureStrings[i], "\0");
-
-                    gesturesCount = 0;
-                }
-            }
-        }
-        //----------------------------------------------------------------------------------
-
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
+            ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
 
-            ClearBackground(RAYWHITE);
+            if (GuiButton((Rectangle){ 24, 24, 120, 30 }, "#191#Show Message")) showMessageBox = true;
 
-            DrawRectangleRec(touchArea, GRAY);
-            DrawRectangle(225, 15, screenWidth - 240, screenHeight - 30, RAYWHITE);
-
-            DrawText("GESTURES TEST AREA", screenWidth - 270, screenHeight - 40, 20, Fade(GRAY, 0.5f));
-
-            for (int i = 0; i < gesturesCount; i++)
+            if (showMessageBox)
             {
-                if (i%2 == 0) DrawRectangle(10, 30 + 20*i, 200, 20, Fade(LIGHTGRAY, 0.5f));
-                else DrawRectangle(10, 30 + 20*i, 200, 20, Fade(LIGHTGRAY, 0.3f));
+                int btnActive = -1;
+                GuiMessageBox((Rectangle){ 85, 70, 250, 100 },
+                    "#191#Message Box", "Hi! This is a message!", "Nice;Cool", &btnActive);
 
-                if (i < gesturesCount - 1) DrawText(gestureStrings[i], 35, 36 + 20*i, 10, DARKGRAY);
-                else DrawText(gestureStrings[i], 35, 36 + 20*i, 10, MAROON);
+                if (btnActive >= 0) showMessageBox = false;
             }
 
-            DrawRectangleLines(10, 29, 200, screenHeight - 50, GRAY);
-            DrawText("DETECTED GESTURES", 50, 15, 10, GRAY);
-
-            if (currentGesture != GESTURE_NONE) DrawCircleV(touchPosition, 30, MAROON);
-
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
+    CloseWindow();
     return 0;
 }
