0f1e83af7efcc13dd798eeb302849cb179f4e173
Fedmichard <francoisfedmichard@gmail.com>
2026-09-24 22:44:43 -0400

    Added left and right panels that auto adjust based on width and height. Made separate funciton declarations. Will start left panel tomorrow. Need to update background colors and panel type.

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,19 @@
 #define RAYGUI_IMPLEMENTATION
 #include "../libs/raygui.h"
 
+// Global constants
+const float width = 1060.f;
+const float height = 540.f;
+
+// Function Declarations
+void DrawLeftPanel();
+
+void DrawRightPanel();
+
+
+// Main Function
 int main()
 {
-    const float width = 1060;
-    const float height = 540;
-
     InitWindow(width, height, "Kairo");
     SetTargetFPS(60);
 
@@ -15,10 +23,14 @@ int main()
     {
         BeginDrawing();
 
-            ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
+            ClearBackground(GetColor(999999)); 
 
-            GuiButton((Rectangle){ 24, 24, 120, 30 }, "Start");
-            GuiButton((Rectangle){ width/10, 24, 120, 30 }, "End");
+            // GuiDisable(); --Can be used later on to disable GUI input?
+            // ------------ LEFT PANEL -----------------
+            DrawLeftPanel();
+
+            // ------------ RIGHT PANEL -----------------
+            DrawRightPanel();
 
         EndDrawing();
     }
@@ -26,3 +38,15 @@ int main()
     CloseWindow();
     return 0;
 }
+
+// Function Definitions
+void DrawRightPanel() {
+    Rectangle bounds { (width/2.f + width/225.f), (height/75.f), (width/2.f) - 10.f, (height/1.005f) - 10.f };
+    GuiWindowBox(bounds, "Right Panel");
+}
+
+void DrawLeftPanel() {
+    Rectangle bounds { (width/125.f), (height/75.f), (width/2.f) - 10.f, (height/1.005f) - 10.f };
+    GuiWindowBox(bounds, "Left Panel");
+}
+
